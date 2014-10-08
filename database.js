267317785e5d004a2bdef26b3c0446d4252fff05
Vison <kk801030@gmail.com>
2014-10-09 02:16:20 +0800

    can do via socket.io

--- database.js
+++ database.js
@@ -9,9 +9,30 @@ var conn = mysql.createConnection({
 	database: 'r03725043',
 	port: 3306
 });
-
+// db connection
 var db_client = conn.connect(function(){
 	console.log('db connected.');
 });
 
-conn.query('INSERT INTO Topic(Topic_Content,Member_ID) values("test","a123")');
+
+// socket.io
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io').listen(http);
+
+io.on('connection', function(socket){
+	console.log('a user connected.');
+	socket.on('post_topic',function(data){
+		console.log('message:'+data);
+		io.emit('update',data);
+	});
+});
+
+http.listen(3005, function(){
+	console.log('listening on *.3005');
+});
+
+function post_topic(content,member){
+	conn.query('INSERT INTO Topic(Topic_Content,Member_ID) values("'+content+'","'+member+'")');
+}
+
