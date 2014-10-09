6bb88f7a1a2cf4e3f266e11de5e23fdbe932b678
Vison <kk801030@gmail.com>
2014-10-09 20:14:37 +0800

    display after load

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
 (function(){
 	var app = angular.module('forum',[]);
-	var _data;
-	app.controller('ContentCtrl',function(){
-		this.data = _data;
-		this.update = function(){
-			this.data = _data;
+	var _data = null;
+	app.controller('ContentCtrl',function($scope,$timeout){
+		$scope.data = 0;
+		$scope.update = function(){
+			//this.data = _data;
 		}
+		var getData = function(obj){
+			if(_data < 10){
+				$timeout(getData,100);
+				$scope.data ++;
+				return;
+			}
+
+			$scope.data = _data;
+			console.log(_data);
+		}
+		
+		$timeout(getData,1000);
+		
 	});
 
+	function get_data(obj){
+		if(_data === null){
+			setTimeout(get_data, 50);
+			return;
+		}
+		return _data;
+	}
 	var socket = io.connect('http://localhost:3005');;
 	var my_id = 'a123';
 	socket.emit('init',my_id);
 	
 	socket.on(my_id,function(data){
-		for(var i=0; i<data.length; i++){
-			console.log(data[i].Topic_Content);
-			
-			for(var j=0; j<data[i].comments.length; j++)
-				console.log("--- "+data[i].comments[j].Comment_Content);
-		}
 		_data = data;
-
+		console.log('get');
+		console.log(ContentCtrl);
+		ContentCtrl.data = _data;
 	});
 
 	$('form').submit(function(){
